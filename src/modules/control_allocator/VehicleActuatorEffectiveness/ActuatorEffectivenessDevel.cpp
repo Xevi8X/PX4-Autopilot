ee5b9c2f862f6d2eecf19cfa5bcdb89dd407b4cf
Wojciech Gajda <wggajda@gmail.com>
2025-08-15 11:31:33 +0200

    update: tilt limits and reduced ct

--- src/modules/control_allocator/VehicleActuatorEffectiveness/ActuatorEffectivenessDevel.cpp
+++ src/modules/control_allocator/VehicleActuatorEffectiveness/ActuatorEffectivenessDevel.cpp
@@ -26,7 +26,7 @@ bool ActuatorEffectivenessDevel::getEffectivenessMatrix(Configuration & configur
 
 	// TODO: load parameters from parameters
 
-	float ct = 5.0f; // thrust coefficient, T = ct * omega^2
+	float ct = 4.0f; // thrust coefficient, T = ct * omega^2
 	float cm = 0.0f; // moment coefficient, M = cm * omega^2
 	auto up_axis = matrix::Vector3f(0.0f, 0.0f, -1.0f); // up axis of the vehicle
 	matrix::Vector3f tilted_forward_axis = matrix::Dcmf(matrix::AxisAnglef(_tilt_axis, _tilt_base)) * up_axis; // forward axis of the front rotors, tilted by the tilt angle
--- src/modules/control_allocator/VehicleActuatorEffectiveness/ActuatorEffectivenessDevel.hpp
+++ src/modules/control_allocator/VehicleActuatorEffectiveness/ActuatorEffectivenessDevel.hpp
@@ -69,7 +69,7 @@ private:
 
 	uORB::SubscriptionData<tilt_forward_s> _tilt_forward_sub{ORB_ID(tilt_forward)};
 
-	static constexpr float _min_tilt_angle{math::radians(-10.0f)}; ///< minimum tilt angle for the front rotors, in radians
-	static constexpr float _max_tilt_angle{math::radians(90.0f)}; ///< maximum tilt angle for the front rotors, in radians
-	static constexpr float _tilt_base_deadzone{math::radians(3.0f)}; ///< deadzone for tilt base updates, in radians
+	static constexpr float _min_tilt_angle{math::radians(-20.0f)}; ///< minimum tilt angle for the front rotors, in radians
+	static constexpr float _max_tilt_angle{math::radians(60.0f)}; ///< maximum tilt angle for the front rotors, in radians
+	static constexpr float _tilt_base_deadzone{math::radians(2.0f)}; ///< deadzone for tilt base updates, in radians
 };
